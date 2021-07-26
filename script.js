e70d7de6769f02cacc6067ced1e8f4b63f825562
Saulo de Castro Bittencourt <saulocastrobitt@gmail.com>
2021-07-26 15:11:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const animais = document.getElementById('animais');
+console.log(animais.innerText);
+
+const gridSection = document.getElementsByClassName('grid-section');
+console.log(gridSection);
+
+const primeiraLi = document.querySelector('li');
+console.log(primeiraLi);
+
+const linkInterno = document.querySelector('[href^="#"]');
+console.log(linkInterno);
+
+const animaisImg = document.querySelectorAll('.animais img');
+console.log(animaisImg[1]);
